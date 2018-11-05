53a9ea44e3732d3cc32fc63ac68309383c744ca7
Emil Levy <emilisonline@gmail.com>
2018-11-05 23:41:49 +1000

    work on doggo tank

--- Character.gd
+++ Character.gd
@@ -1,7 +1,16 @@
 extends KinematicBody2D
 
-var motion = Vector2()
 var gravity = 10
+
+var firing = false
+
+export (float) var chargeRate = 200
+
+var charging = false
+var chargePower = 0
+var chargeDown = false
+
+var motion = Vector2()
 
 # export vars
 export (int) var movementSpeed
@@ -10,15 +19,34 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+	
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if !charging:
+			chargePower = 0
+			charging = true
+			$DoggoTank.isActive = false
+	
+	else:
+		if charging:
+			print("fire projectile with power: ", chargePower)
+			charging = false
+		$DoggoTank.isActive = true
+	
+	handle_charge(delta)
+	
 
 func _physics_process(delta):
+	handle_movement(delta)
+	
+func handle_movement(delta):	
 	motion.y += gravity
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && !charging:
 		motion.x = movementSpeed 
 		$DoggoTank.moving = true
 		$DoggoTank.backwards = true
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") && !charging:
 		motion.x = -movementSpeed 
 		$DoggoTank.moving = true
 		$DoggoTank.backwards = false
@@ -30,3 +58,29 @@ func _physics_process(delta):
 	if collision:
 		motion = motion.slide(collision.normal)
 	
+func drawChargeBar():
+	$ChargeBorder.visible = true
+	$ChargeBar.visible = true
+	$ChargeBar.scale = Vector2(chargePower / 100, 1)
+	
+func hideChargeBar():
+	$ChargeBorder.visible = false
+	$ChargeBar.visible = false
+	
+func handle_charge(delta):
+	if charging:
+		if chargeDown:
+			chargePower -= chargeRate * delta
+		else:
+			chargePower += chargeRate * delta
+			
+		chargePower = clamp(chargePower, 0, 100)
+				
+		if chargePower == 100:
+			chargeDown = true
+		elif chargePower == 0:
+			chargeDown = false
+		
+		drawChargeBar()
+	else:
+		hideChargeBar()
--- DoggoTank.gd
+++ DoggoTank.gd
@@ -5,12 +5,18 @@ export (bool) var backwards
 export (int) var jiggleFrames = 10
 export (int) var jigglePixels = 5
 
+export (bool) var isActive = false
+
 var jiggleCount = 0
 
 func _ready():
 	pass
 
 func _process(delta):
+	if !isActive:
+		$Treads.play('stopped')
+		return
+	
 	if moving:
 		$Treads.play('moving')
 		shouldJiggle()
@@ -18,24 +24,47 @@ func _process(delta):
 		$Treads.play('stopped')
 	
 	flippo(backwards)
+	
+	aim_Barrel()
 
 func shouldJiggle():
 	jiggleCount+=1
 	var jigglo = jiggleCount % (jiggleFrames *2)
-	var offset = $Doggo.offset
 	
 	if jigglo == jiggleFrames:
-		$Doggo.offset = Vector2(offset.x, 0)
+		$Doggo.offset = Vector2($Doggo.offset.x, 0)
+		$Doggo/Turret.offset = Vector2($Doggo/Turret.offset.x, 0)
+		
 	if jigglo == 0:
 		jiggleCount = 0
-		$Doggo.offset = Vector2(offset.x, jigglePixels)
+		$Doggo.offset = Vector2($Doggo.offset.x, jigglePixels)
+		$Doggo/Turret.offset = Vector2($Doggo/Turret.offset.x, jigglePixels)
 		
-func flippo(flip):	
+func flippo(flip):
 	$Doggo.set_flip_h(flip)
 	$Treads.set_flip_h(flip)
 	$Doggo/Turret.set_flip_h(flip)
 	
 	if flip:
 		$Doggo.offset = Vector2(60, $Doggo.offset.y)
+		
 	else:
 		$Doggo.offset = Vector2(0, $Doggo.offset.y)
+		
+func aim_Barrel():
+	var mousePos = get_global_mouse_position()
+	
+	var offset = 180
+	if backwards:
+		offset = 270
+	
+	var lookAngle = rad2deg(mousePos.angle_to_point($Doggo/Barrel.global_position)) + 360 
+	
+	## add some math trickery so the crossover point is dead under the tank.
+	if lookAngle > 450:
+		lookAngle -= 360
+	
+	var clampedAngl = deg2rad(clamp(lookAngle, offset, 90 + offset))
+	
+	$Doggo/Barrel.set_rotation(clampedAngl)
+	
